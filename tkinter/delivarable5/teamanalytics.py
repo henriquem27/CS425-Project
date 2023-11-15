95c08c357268fe320fbba85858556f25e022cd45
Henrique Rio <henriquerio27@gmail.com>
2023-11-15 12:03:06 -0600

    Team done for now

--- tkinter/delivarable5/teamanalytics.py
+++ tkinter/delivarable5/teamanalytics.py
@@ -20,6 +20,8 @@ def generate_teamsanalytics(tabhome,root):
 
 
     Label(tabhome,text='Select the Team(s) and Click a button to display the query:').pack(anchor='w')
+    
+    Season = Entry(tabhome)
 
     def clicked():
         print("clicked")
@@ -31,7 +33,7 @@ def generate_teamsanalytics(tabhome,root):
     def get_teams():
         conn = get_conn()
         cursor = conn.cursor()
-        cursor.execute("SELECT DISTINCT Team from Teams;")
+        cursor.execute("SELECT DISTINCT Team from Teams ORDER BY Team;")
         records= cursor.fetchall()
         return records
     
@@ -92,149 +94,6 @@ def generate_teamsanalytics(tabhome,root):
     Label(tabhome,text='Shows total Points by Season and the total:').pack(anchor='w')
     Button(tabhome, text="Total Points by Season", width=20,
         command=lambda: points_rollup()).pack(anchor='w')
-    
-
-    def team_budgets():
-        conn = get_conn()
-
-        try:
-            top_window = Toplevel(root)
-            top_window.title('Team Budgets')
-            top_window.geometry('400x400')
-            popCanv = Canvas(top_window, width=600, height=300,
-                             scrollregion=(0, 0, 2000, 2000))  # width=1256, height = 1674)
-            frame = Frame(popCanv)
-            ksbar = Scrollbar(top_window, orient=VERTICAL,
-                              command=popCanv.yview)
-            popCanv.configure(yscrollcommand=ksbar.set)
-            
-            cursor = conn.cursor()
-            selected = box.curselection()
-            teamnames = ()
-            for idx in selected:
-                teamnames += (box.get(idx),)
-
-            QUERY = "SELECT Team,Season_ID,team_budget, SUM(TEAM_BUDGET.Team_Budget) OVER (PARTITION BY team order by season_id ROWS UNBOUNDED PRECEDING) FROM team_budget WHERE team IN %s ORDER BY Team,Season_ID;"
-            cursor.execute(QUERY, (teamnames,))
-            records = cursor.fetchall()
-            Label(frame,text="Team",background='grey').grid(row=0,column=0,padx=5)
-            Label(frame,text="Season",background='grey').grid(row=0, column=1,padx=5)
-            Label(frame, text="Budget", background='grey').grid(
-                row=0, column=2, padx=5)
-            Label(frame, text="Budget Running Total", background='grey').grid(
-                row=0, column=3, padx=5)
-            ttk.Separator(frame, orient=HORIZONTAL).grid(
-                    row=0, columnspan=2, sticky='ns')
-            ttk.Separator(frame, orient=HORIZONTAL).grid(
-                    row=0, columnspan=3, sticky='ns')
-            ttk.Separator(frame, orient=VERTICAL).grid(
-                row=0, column=3, columnspan=1, rowspan=2, sticky='e')
-            
-            for x in range(len(records)):
-                Label(frame, text=records[x][0]).grid(
-                    row=x+1, column=0, padx=5)
-                Label(frame, text=records[x][1]).grid(
-                    row=x+1, column=1, padx=5)
-                Label(frame, text=records[x][2]).grid(
-                    row=x+1, column=2, padx=5)
-                Label(frame, text=records[x][3]).grid(
-                    row=x+1, column=3, padx=5)
-                ttk.Separator(frame, orient=HORIZONTAL).grid(
-                    row=x+1, columnspan=2, sticky='ns')
-                ttk.Separator(frame, orient=HORIZONTAL).grid(
-                    row=x+1, columnspan=3, sticky='ns')
-                ttk.Separator(frame, orient='horizontal').grid(
-                    row=x+1, column=0, columnspan=5, ipadx=350, sticky='s')
-            
-            ksbar.pack(side="right", fill="y")
-            popCanv.pack(side="left", fill="both", expand=True)
-            popCanv.create_window((4, 4), window=frame, anchor="nw")
-            conn.commit()
-
-        except Exception:
-            traceback.print_exc()
-            MessageBox.showerror(
-                "OPS", " something went wrong. Please make sure you have at least one team selected")
-
-        finally:
-            conn.close()
-    
-    Label(tabhome, text='Shows the team budgets by season with a running total:').pack(
-        anchor='w')
-    Button(tabhome, text="Team Budget", width=20,
-           command=lambda: team_budgets()).pack(anchor='w')
-
-    def Home_Away_avg2():
-        conn = get_conn()
-
-        try:
-            top_window = Toplevel(root)
-            top_window.title('Average Home-Away Performance')
-            top_window.geometry('700x400')
-            popCanv = Canvas(top_window, width=600, height = 300,
-                             scrollregion=(0, 0, 3000, 3000))  # width=1256, height = 1674)
-            frame = Frame(popCanv)
-            ksbar = Scrollbar(top_window, orient=VERTICAL,
-                              command=popCanv.yview)
-            popCanv.configure(yscrollcommand = ksbar.set)
-            
-            
-            
-            cursor = conn.cursor()
-            selected = box.curselection()
-            teamnames = ()
-            for idx in selected:
-                teamnames += (box.get(idx),)
-
-            QUERY = "SELECT x.Team,x.Season_ID as Season,x.HomeAVG,y.AwayAvg, X.HomeAVG-y.AwayAvg as AVG_GOAL_DIFFERENTIAl FROM "
-            QUERY=QUERY+"(SELECT t.team,ROUND(AVG(g.Homegoals),4) as HomeAVG,t.Season_ID,g.home_team_id FROM teams t,games g WHERE t.team_id=g.home_team_id "
-            QUERY=QUERY+" GROUP BY t.team_id,g.home_team_id ORDER BY Team_ID) as x,(SELECT t.team,ROUND(AVG(g.AwayGoals),4) as AwayAvg,g.Season_ID,g.away_team_id FROM teams t,games g WHERE t.team_id=g.away_team_id "
-            QUERY=QUERY+" GROUP BY t.team_id,g.away_team_id,g.Season_ID ORDER BY Team_ID) as y WHERE x.Home_Team_ID=y.Away_team_ID AND x.team IN %s;"
-            cursor.execute(QUERY, (teamnames,))
-            records = cursor.fetchall()
-            Label(frame, text="Team", background='grey').grid(
-                row=0, column=0, padx=5)
-            Label(frame, text="Season", background='grey').grid(
-                row=0, column=1, padx=5)
-            Label(frame, text="Average Home Goals", background='grey').grid(
-                row=0, column=2, padx=5)
-            Label(frame, text="Average Away Goals", background='grey').grid(
-                row=0, column=3, padx=5)
-            Label(frame, text="Average Home-Away Goal Differrential", background='grey').grid(
-                row=0, column=4, padx=5)
-            
-            
-            for x in range(len(records)):
-                Label(frame, text=records[x][0]).grid(
-                    row=x+1, column=0, padx=5)
-                Label(frame, text=records[x][1]).grid(
-                    row=x+1, column=1, padx=5)
-                Label(frame, text=records[x][2]).grid(
-                    row=x+1, column=2, padx=5)
-                Label(frame, text=records[x][3]).grid(
-                    row=x+1, column=3, padx=5)
-                Label(frame, text=records[x][4]).grid(
-                    row=x+1, column=4, padx=5)
-                ttk.Separator(frame, orient='horizontal').grid(row=x+1,column=0,columnspan=5,ipadx=350,sticky='s')
-
-                
-            ksbar.pack(side="right", fill="y")
-            popCanv.pack(side="left", fill="both", expand=True)
-            popCanv.create_window((4,4), window=frame, anchor="nw")  
-            conn.commit()
-
-        except Exception:
-            traceback.print_exc()
-            MessageBox.showerror(
-                "OPS", " something went wrong. Please make sure you have at least one team selected")
-
-        finally:
-            conn.close()
-
-    Label(tabhome, text='Displays the Average Home-Away Performance in terms of goal:').pack(
-        anchor='w')
-    Button(tabhome, text="Home-Away-Performance", width=20,
-           command=lambda: Home_Away_avg2()).pack(anchor='w')  
     
     def team_budgets2():
         conn = get_conn()
@@ -378,10 +237,77 @@ def generate_teamsanalytics(tabhome,root):
         finally:
             conn.close()
 
-    Label(tabhome, text='Shows the team budgets by season with a running total:').pack(
+    Label(tabhome, text='Displays the Average Home-Away Performance in terms of goal:').pack(
         anchor='w')
     Button(tabhome, text="Avg Home-Away Performance", width=20,
            command=lambda: home_away_perf()).pack(anchor='w')
+    
+    def top_scores():
+        conn = get_conn()
 
+        try:
+            top_window = Toplevel(root)
+            top_window.title('Team Budgets')
+            top_window.geometry('520x400')
+            tree_frame = Frame(top_window)
+            tree_frame.pack(fill='both', expand=1, pady=20)
+            tree_scroll = Scrollbar(tree_frame)
+            tree_scroll.pack(side=RIGHT, fill=Y)
+            my_tree = ttk.Treeview(
+                tree_frame, yscrollcommand=tree_scroll.set, selectmode="extended")
 
+            my_tree.pack(fill='both', expand=1)
+
+            tree_scroll.config(command=my_tree.yview)
+
+            my_tree['columns'] = (
+                "Team", "Season", "Total Budget", "Running Total")
+            my_tree.column("#0", width=0, stretch=NO)
+            my_tree.column("Team", anchor=CENTER, width=50)
+            my_tree.column("Season", anchor=CENTER, width=60)
+            my_tree.column("Total Budget", anchor=CENTER, width=140)
+            my_tree.column("Running Total", anchor=CENTER, width=140)
+
+            # Create Headings
+            my_tree.heading("#0", text="", anchor=W)
+            my_tree.heading("Team", text="Team", anchor=CENTER)
+            my_tree.heading("Season", text="Season", anchor=CENTER)
+            my_tree.heading("Total Budget", text="Team Budget", anchor=CENTER)
+            my_tree.heading("Running Total",
+                            text="Running Total", anchor=CENTER)
+
+            my_tree.tag_configure('oddrow', background="#084370")
+            my_tree.tag_configure('evenrow')
+
+            cursor = conn.cursor()
+            selected = box.curselection()
+            teamnames = ()
+            for idx in selected:
+                teamnames += (box.get(idx),)
+
+            QUERY = "SELECT Team,Season_ID,team_budget, SUM(TEAM_BUDGET.Team_Budget) OVER (PARTITION BY team order by season_id ROWS UNBOUNDED PRECEDING) FROM team_budget WHERE team IN %s ORDER BY Team,Season_ID;"
+            cursor.execute(QUERY, (teamnames,))
+            records = cursor.fetchall()
+            x = 0
+
+            for x in range(len(records)):
+                if x % 2 == 0:
+                    my_tree.insert(parent='', index='end', iid=x, text="", values=(
+                        records[x][0], records[x][1], records[x][2], records[x][3]), tags=('evenrow',))
+                else:
+                    my_tree.insert(parent='', index='end', iid=x, text="", values=(
+                        records[x][0], records[x][1], records[x][2], records[x][3]), tags=('oddrow',))
+
+        except Exception:
+            traceback.print_exc()
+            MessageBox.showerror(
+                "OPS", " something went wrong. Please make sure you have at least one team selected")
+
+        finally:
+            conn.close()
+
+    Label(tabhome, text='Shows the team budgets by season with a running total:').pack(
+        anchor='w')
+    Button(tabhome, text="Team Budget2", width=20,
+           command=lambda: team_budgets2()).pack(anchor='w')
 
